9bff1c8665bbea80e6acec0b2c46d5db58d00ba3
SIR CALCULUS <calculus2uk@gmail.com>
2018-05-26 22:25:19 +0300

    Add error handling files

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #Configuration for email server
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'email@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'password'
+    ADMINS = ['email@gmail.com']
